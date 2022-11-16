ca4c3f432d8f613d8c896661d38a277355fc53a5
Daniil Chemaev <chemaev.ds@gmail.com>
2022-11-16 22:54:22 +0300

    Added empty TableView & Deleted Main.storyboard

--- HTTP-Requests/HTTP-Requests/ViewController.swift
+++ HTTP-Requests/HTTP-Requests/ViewController.swift
@@ -9,17 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let tableView: UITableView = .init(frame: .zero, style: .insetGrouped)
     let session = URLSession.shared
     var posts: [Post] = []
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Task {
-            await obtainPosts()
-        }
+
+        setupUI()
+        setup()
     }
+    func setupUI() {
+        view.backgroundColor = .red
+
+        navigationItem.title = "Title"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        view.addSubview(tableView)
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func setup() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "text")
+        tableView.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell(style: .default, reuseIdentifier: "text")
+    }
+
 
     func obtainPosts() async {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
@@ -31,11 +67,12 @@ class ViewController: UIViewController {
             decoder.keyDecodingStrategy = .convertFromSnakeCase
             let response = try decoder.decode([Post].self, from: data)
             print("posts: \(response)")
-            
+
 
         } catch {
             print("Error: \(String(describing: error))")
         }
     }
 }
+
 
---
+++ Json-parser/Json-parser/TableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TableViewController.swift
+//  Json-parser
+//
+//  Created by Daniil Chemaev on 16.11.2022.
+//
+
+import Foundation
